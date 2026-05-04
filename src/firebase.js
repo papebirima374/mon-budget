ed1f1858ce49f4338459b79ccc51f0db6638f69d
papebirima374 <papebirima374@email.com>
2026-05-04 02:09:00 +0000

    Fix: Ajout export db

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Ajout de cette ligne
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDbs920iWKV2w18QyGZYdYU4yWEpTurd44",
   authDomain: "mon-budget-app-5cf97.firebaseapp.com",
@@ -13,5 +10,8 @@ const firebaseConfig = {
   appId: "1:842290924565:web:6246099ea8798ea7c0d21c"
 };
 
-// Initialize Firebase
+// Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialisation de Firestore (La base de données) et EXPORT
+export const db = getFirestore(app);
